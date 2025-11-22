dea5a72ae5eb21c2fa3199bea6d40a69df4d965b
MCegla-JW <m.cegla93@gmail.com>
2025-11-22 15:39:37 +0000

    added password validation for user model

--- models/user.js
+++ models/user.js
@@ -7,6 +7,19 @@ const userSchema = new mongoose.Schema({
     password: { type: String, required: true, unique: true},
 })
 
+userSchema
+.virtual('confirmPassword')
+.set(function(passwordValue) {
+    this._confirmPassword = passwordValue
+})
+
+userSchema.pre('validate', function(next){
+    if (this.isModified('password') && this.password !== this._confirmPassword){
+        this.invalidate('confirmPassword', 'Passwords do not match')
+    }
+    next()
+})
+
 userSchema.pre('save', function(next){
     if (this.isModified('password')){
     this.password = bcrypt.hashSync(this.password, 12)
