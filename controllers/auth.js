2791d4a15b32902c304a01ad08de7fd71377e54e
MCegla-JW <m.cegla93@gmail.com>
2025-11-24 11:39:57 +0000

    created a jwt token on sign-in router

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ import express from 'express'
 import User from '../models/user.js'
 import bcrypt from 'bcrypt'
 import { Unauthorized, NotFound, BadRequest } from '../utils/erros.js'
+import jwt from 'jsonwebtoken'
 
 const router = express.Router()
 
@@ -44,7 +45,12 @@ router.post('/sign-in', async (req, res) => {
     if (!passwordMatch) {
         throw new Unauthorized('Unauthorized')
     }
-    return res.json({ message: 'User signed in'})
+    const token = jwt.sign(
+        { user: { _id: userToLogIn._id, username: userToLogIn.username} },
+        process.env.TOKEN_SECRET, 
+        { expiresIn: '2d'}
+    )
+    return res.json({token})
     } catch (error) {
     console.log(error.message)
     next(error)
